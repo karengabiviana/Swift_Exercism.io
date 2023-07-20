fffc0ea3fc8d5f833383489615ec806d150efbb0
Karen Gabrieli <karen.oliveira@sumup.com>
2023-07-20 15:33:54 -0300

    Create Project

---
+++ ExercismWithTests/ExercismWithTests/ExercismWithTests.h
@@ -0,0 +1,18 @@
+//
+//  ExercismWithTests.h
+//  ExercismWithTests
+//
+//  Created by Karen Oliveira on 20/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExercismWithTests.
+FOUNDATION_EXPORT double ExercismWithTestsVersionNumber;
+
+//! Project version string for ExercismWithTests.
+FOUNDATION_EXPORT const unsigned char ExercismWithTestsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExercismWithTests/PublicHeader.h>
+
+
