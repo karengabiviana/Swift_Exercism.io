ae71538abe3a138e9dc1e927fbedcf25143d250e
Karen Gabrieli <karen.oliveira@sumup.com>
2023-11-28 17:47:51 -0300

    18 of 99 - Exercise

---
+++ ExercismWithTests/ExercismWithTestsTests/LogLines.swift
@@ -0,0 +1,95 @@
+//
+//  LogLines.swift
+//  ExercismWithTestsTests
+//
+//  Created by Karen Oliveira on 06/11/23.
+//
+
+import XCTest
+
+//Instructions
+//In this exercise you'll be processing log-lines.
+//
+//Each log line is a string formatted as follows: "[<LVL>]: <MESSAGE>".
+//
+//These are the different log levels:
+//
+//TRC (trace)
+//DBG (debug)
+//INF (info)
+//WRN (warning)
+//ERR (error)
+//FTL (fatal)
+//You have three tasks, each of which need to be completed to provide the necessary functionality.
+
+//Task 1
+//Define a LogLevel enum that has six cases corresponding to the above log levels, plus an unknown case for levels with missing or non-standard log levels.
+//
+//trace
+//debug
+//info
+//warning
+//error
+//fatal
+//unknown
+enum LogLevel: Int {
+    case trace = 0
+    case debug = 1
+    case info = 4
+    case warning = 5
+    case error = 6
+    case fatal = 7
+    case unknown = 42
+
+    //Task 2
+    //Next, implement the LogLevel initializer to parse the log level of a log line:
+    //LogLevel("[INF]: File deleted")
+    // // => LogLevel.info
+    //LogLevel("Something went wrong!")
+    // // => LogLevel.unknown
+    init?(_ input: String) {
+        let logLevel = input.split(separator: ":").dropLast()
+
+        switch(logLevel.first) {
+        case "[TRC]":
+            self = .trace
+        case "[DBG]":
+            self = .debug
+        case "[INF]":
+            self = .info
+        case "[WRN]":
+            self = .warning
+        case "[ERR]":
+            self = .error
+        case "[FTL]":
+            self = .fatal
+        default:
+            self = .unknown
+            break
+        }
+    }
+    //Task 3
+    func shortFormat(message: String) -> String {
+
+        switch(self) {
+        case .trace:
+            return "0:" + message
+        case .debug:
+            return "1:" + message
+        case .info:
+            return "4:" + message
+        case .warning:
+            return "5:" + message
+        case .error:
+            return "6:" + message
+        case .fatal:
+            return "7:" + message
+        case .unknown:
+            return "42:" + message
+        }
+    }
+}
+
+class LogLinesTests: XCTestCase {
+
+}
