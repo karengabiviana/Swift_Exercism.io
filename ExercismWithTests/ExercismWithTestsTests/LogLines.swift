f3cca91ae4e516be099752ab99252a45d20b98ca
Karen Gabrieli <karen.oliveira@sumup.com>
2023-11-29 15:08:02 -0300

    18 of 99 - Tests of ShortFormat

--- ExercismWithTests/ExercismWithTestsTests/LogLines.swift
+++ ExercismWithTests/ExercismWithTestsTests/LogLines.swift
@@ -119,4 +119,32 @@ class LogLinesTests: XCTestCase {
         let line = "[XYZ]: Gibberish message.. beep boop.."
         XCTAssertEqual(LogLevel(line), LogLevel.unknown)
     }
+    func testShortTrace() {
+        let message = "Line 13 - int myNum = 42;"
+        XCTAssertEqual(LogLevel.trace.shortFormat(message: message), "0:Line 13 - int myNum = 42;")
+    }
+    func testShortDebug() {
+        let message = "The name 'LogLevel' does not exist in the current context"
+        XCTAssertEqual(LogLevel.debug.shortFormat(message: message),"1:The name 'LogLevel' does not exist in the current context")
+    }
+    func testShortInfo() {
+        let message = "File moved"
+        XCTAssertEqual(LogLevel.info.shortFormat(message: message), "4:File moved")
+    }
+    func testShortWarning() {
+        let message = "Unsafe password"
+        XCTAssertEqual(LogLevel.warning.shortFormat(message: message), "5:Unsafe password")
+    }
+    func testShortError() {
+        let message = "Stack overflow"
+        XCTAssertEqual(LogLevel.error.shortFormat(message: message), "6:Stack overflow")
+    }
+    func testShortFatal() {
+        let message = "Dumping all files"
+        XCTAssertEqual(LogLevel.fatal.shortFormat(message: message), "7:Dumping all files")
+    }
+    func testShortUnknownEmpty() {
+        let message = "Wha happon?"
+        XCTAssertEqual(LogLevel.unknown.shortFormat(message: message), "42:Wha happon?")
+    }
 }
