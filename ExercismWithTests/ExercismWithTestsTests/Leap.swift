c641ce31961e585d78d76778b66d4c969583f261
Karen Gabrieli <karengabrieli.viana@outlook.com>
2024-01-15 10:28:35 -0300

    20 of 99 - Add exercise structure

---
+++ ExercismWithTests/ExercismWithTestsTests/Leap.swift
@@ -0,0 +1,27 @@
+//
+//  Leap.swift
+//  ExercismWithTestsTests
+//
+//  Created by Karen Oliveira on 15/01/24.
+//
+
+import Foundation
+import XCTest
+
+//*Instructions*
+//Given a year, report if it is a leap year.
+//
+//The tricky thing here is that a leap year in the Gregorian calendar occurs:
+//on every year that is evenly divisible by 4
+//  except every year that is evenly divisible by 100
+//    unless the year is also evenly divisible by 400
+
+//For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+
+class Year {
+    // Write your code for the 'Leap' exercise in this file.
+}
+
+class LeapTests: XCTestCase {
+    // Write your tests for the 'Leap' exercise in this file.
+}
