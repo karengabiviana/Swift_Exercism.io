b6717a93511849260123cc40932705c42e226f70
Karen Gabrieli <karengabrieli.viana@outlook.com>
2024-02-02 08:59:55 -0300

    20of99 Refactor

--- ExercismWithTests/ExercismWithTestsTests/Leap.swift
+++ ExercismWithTests/ExercismWithTestsTests/Leap.swift
@@ -53,7 +53,7 @@ class LeapTests: XCTestCase {
 
     func testIsNoLeap() {
         //Arrange
-        let year = Year()
+        let year = Year(calendarYear: <#Int#>)
         let input = 1997
         //Act
         let result = year.isLeap(year: input)
@@ -63,7 +63,7 @@ class LeapTests: XCTestCase {
 
     func testIsNoLeapDivisibleBy100() {
         //Arrange
-        let year = Year()
+        let year = Year(calendarYear: <#Int#>)
         let input = 1900
         //Act
         let result = year.isLeap(year: input)
@@ -72,7 +72,7 @@ class LeapTests: XCTestCase {
     }
     func testIsLeapDivisibleBy400() {
         //Arrange
-        let year = Year()
+        let year = Year(calendarYear: <#Int#>)
         let input = 2000
         //Act
         let result = year.isLeap(year: input)
